e48105cc72ce4c24cd395966fbf58f7ccbbcbb91
ahihiyou20 <95456402+ahihiyou20@users.noreply.github.com>
2022-09-18 10:52:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from sys import executable, argv
 from signal import signal, SIGINT
 from time import sleep, strftime, localtime, time
 from datetime import timedelta
-from requests import get
 import atexit
 import random
 from re import findall
 from base64 import b64encode
 import json
+from webbrowser import open as open_browser
 
 from menu import UI
 from color import color
@@ -19,6 +19,7 @@ from gems import gems
 from exception import exception
 from api import CAPI
 try:
+	from requests import get
 	from inputimeout import inputimeout, TimeoutOccurred
 	import discum
 	from discum.utils.slash import SlashCommander
@@ -26,6 +27,7 @@ try:
 except:
 	from setup import install
 	install()
+	from requests import get
 	from inputimeout import inputimeout, TimeoutOccurred
 	import discum
 	from discum.utils.slash import SlashCommander
@@ -35,20 +37,19 @@ wbm=[14,17]
 ui = UI()
 client = data()
 
-def signal_handler(sig, frame):
+def signal_handler(sig: object, frame: object):
 	sleep(0.5)
 	ui.slowPrinting(f"\n{color.fail}[INFO] {color.reset}Detected Ctrl + C, Stopping...")
 	raise KeyboardInterrupt
 
 signal(SIGINT, signal_handler)
+
 bot = discum.Client(token=client.token, log=False, user_agent=[
  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36/PAsMWa7l-11',
  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 YaBrowser/20.8.3.115 Yowser/2.5 Safari/537.36',
  'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.7.2) Gecko/20100101 / Firefox/60.7.2'])
 
-client.guildID = bot.getChannel(client.channel).json()['guild_id']
-
-def slash(command):
+def slash(command: str) -> None:
         slashCmds = bot.getSlashCommands(client.OwOID).json()
         s = SlashCommander(slashCmds, application_id=client.OwOID)
         data = s.get([command])
@@ -82,12 +83,34 @@ while True:
 		ui.slowPrinting(f'{color.fail} !! [ERROR] !! {color.reset} Wrong input!')
 		sleep(1)
 
-def at():
+def at() -> str:
 	return f'\033[0;43m{strftime("%d %b %Y %H:%M:%S", localtime())}\033[0;21m'
 
+def getMessages(num: int=1, channel: str=client.channel) -> object:
+	messageObject = None
+	retries = 0
+	while not messageObject:
+		if not retries > 10:
+			messageObject = bot.getMessages(channel, num=num)
+			messageObject = messageObject.json()
+			if not type(messageObject) is list:
+				messageObject = None
+			else:
+				break
+			retries += 1
+			continue
+		if type(messageObject) is list:
+			break
+		else:
+			retries = 0
+	return messageObject
 @bot.gateway.command
-def on_ready(resp):
-	if resp.event.ready_supplemental: #ready_supplemental is sent after ready
+def on_ready(resp: object) -> None:
+	if resp.event.ready_supplemental:
+		client.guildID = bot.getChannel(client.channel).json()['guild_id']
+		for i in range(len(bot.gateway.session.DMIDs)):
+			if client.OwOID in bot.gateway.session.DMs[bot.gateway.session.DMIDs[i]]['recipients']:
+				client.dmsID = bot.gateway.session.DMIDs[i]
 		user = bot.gateway.session.user
 		ui.slowPrinting(f"Logged in as {user['username']}#{user['discriminator']}")
 		ui.slowPrinting('══════════════════════════════════════')
@@ -103,53 +126,43 @@ def on_ready(resp):
 		ui.slowPrinting(f"Webhook: {'YES' if client.webhook['link'] != 'None' else 'NO'}")
 		ui.slowPrinting(f"Webhook Ping: {client.webhook['ping']}")
 		ui.slowPrinting(f"Daily Mode: {client.daily}")
-		ui.slowPrinting(f"{'Stop After (Seconds)' if client.stop.isdigit() else 'Stop Mode'}: {client.stop}")
+		ui.slowPrinting(f"{'Stop After (Seconds)' if client.stop and client.stop.isdigit() else 'Stop Mode'}: {client.stop}")
 		ui.slowPrinting(f"Sell Mode: {client.sell['enable']}")
 		ui.slowPrinting(f"Solve Captcha Mode: {client.solve['enable']}")
-		ui.slowPrinting(f"+)Captcha Solving Server: {'https://autofarmsupport.tk' if client.solve['server'] == '1' else 'https://afbot.dev'}{color.reset}")
+		ui.slowPrinting(f"+)Captcha Solving Server: {'https://autofarmsupport.tk' if client.solve['server'] == 1 else 'https://afbot.dev'}{color.reset}")
 		ui.slowPrinting('══════════════════════════════════════')
 		loopie()
-def webhookPing(message):
-	if client.webhook['link'] != 'None':
+
+def webhookPing(message: str) -> None:
+	if client.webhook['link']:
 		webhook = DiscordWebhook(url = client.webhook['link'], content=message)
 		webhook = webhook.execute()
 
 @bot.gateway.command
-def security(resp):
-	if issuechecker(resp) == "solved":
-		if client.webhook['ping'] != 'None':
+def security(resp: object) -> None:
+	result = None
+	if resp.event.message:
+		result = issuechecker(resp)
+	if result and result == "solved":
+		if client.webhook['ping']:
 			webhookPing(f"<@{client.webhook['ping']}> Solved A Captcha In Channel: <#{client.channel}>")
 		else:
-			user = bot.gateway.session.user
-			webhookPing(f"<@{user['id']}> <@{client.webhook['allowedid']}> Solved A Captcha In Channel: <#{client.channel}>")
+			webhookPing(f"<@{client.webhook.get('allowedid', bot.gateway.session.user['id'])}> Solved A Captcha In Channel: <#{client.channel}>")
 		ui.slowPrinting(f'{color.okcyan}[INFO] {color.reset}Captcha Solved. Starting To Run Again')
 		sleep(2)
 		execl(executable, executable, *argv)
-	if issuechecker(resp) == "captcha":
+	elif result == "captcha":
 		client.stopped = True
-		if client.webhook['ping'] != 'None':
+		if client.webhook['ping']:
 			webhookPing(f"<@{client.webhook['ping']}> I Found A Captcha In Channel: <#{client.channel}>")
 		else:
-			user = bot.gateway.session.user
-			webhookPing(f"<@{user['id']}> <@{client.webhook['allowedid']}> I Found A Captcha In Channel: <#{client.channel}>")
+			webhookPing(f"<@{client.webhook.get('allowedid', bot.gateway.session.user['id'])}> I Found A Captcha In Channel: <#{client.channel}>")
 		bot.switchAccount(client.token[:-4] + 'FvBw')
 
-@bot.gateway.command
-def issuechecker(resp):
-	try:
-		user = bot.gateway.session.user
-		def dms():
-				i = 0
-				length = len(bot.gateway.session.DMIDs)
-				while i < length:
-					if client.OwOID in bot.gateway.session.DMs[bot.gateway.session.DMIDs[i]]['recipients']:
-						return bot.gateway.session.DMIDs[i]
-					else:
-						i += 1
-		dmsid = dms()
-	except:
-		dmsid = None
-	def solve(image_url, msgs):
+def issuechecker(resp: object) -> str:
+	m = resp.parsed.auto()
+	user = bot.gateway.session.user
+	def solve(image_url: str, msgs: str) -> str:
 		client.stopped = True
 		api = CAPI(user['id'], client.solve['server'])
 		encoded_string = b64encode(get(image_url).content).decode('utf-8')
@@ -157,12 +170,13 @@ def issuechecker(resp):
 		r = api.solve(Json={'data': encoded_string, 'len': msgs[msgs.find("letter word") - 2]})
 		if r:
 			ui.slowPrinting(f"{color.okcyan}[INFO] {color.reset}Solved Captcha [Code: {r['code']}]")
-			bot.sendMessage(dmsid, r['code'])
+			bot.sendMessage(client.dmsID, r['code'])
 			sleep(10)
-			msgs = bot.getMessages(dmsid)
+			msgs = getMessages(channel=client.dmsID)
 			try:
-				msgs = json.loads(msgs.text[1:-1]) if type(msgs.json()) is list else {'author': {'id': '0'}}
-			except:
+				msgs = msgs[0]
+			except Exception as e:
+				print(str(e))
 				ui.slowPrinting(f"{color.okcyan}[INFO] {color.reset}There's An Issue With Rerunner")
 				sleep(2)
 				return "captcha"
@@ -180,103 +194,93 @@ def issuechecker(resp):
 		else:
 			ui.slowPrinting(f"{color.okcyan}[INFO] {color.reset}Captcha Solver API Is Having An Issue...")
 			return "captcha"
-	if resp.event.message:
-		m = resp.parsed.auto()
-		if m['channel_id'] == client.channel or m['channel_id'] == dmsid and not client.stopped:
+	if m['channel_id'] == client.channel or m['channel_id'] == client.dmsID and not client.stopped:
 			if m['author']['id'] == client.OwOID or m['author']['username'] == 'OwO' or m['author']['discriminator'] == '8456' and bot.gateway.session.user['username'] in m['content'] and not client.stopped:
 				if 'banned' in m['content'].lower() and not client.stopped:
 					ui.slowPrinting(f'{at()}{color.fail} !!! [BANNED] !!! {color.reset} Your Account Have Been Banned From OwO Bot Please Open An Issue On The Support Discord server')
 					return "captcha"
 				if any(captcha in m['content'].lower() for captcha in ['(1/5)', '(2/5)', '(3/5)', '(4/5)', '(5/5)']) and not client.stopped:
-					msgs=bot.getMessages(dmsid)
-					msgs=msgs.json()
-					if type(msgs) is dict:
-						ui.slowPrinting(f'{at()}{color.warning} !! [CAPTCHA] !! {color.reset} ACTION REQUİRED')
-						return "captcha"
+					msgs=getMessages(channel=client.dmsID)
+					print(msgs)
 					if msgs[0]['author']['id'] == client.OwOID and '⚠'  in msgs[0]['content'] and msgs[0]['attachments'] and not client.stopped:
 						ui.slowPrinting(f'{at()}{color.warning} !! [CAPTCHA] !! {color.reset} ACTION REQUİRED')
-						if client.solve['enable'].lower() != "no" and not client.stopped:
+						if client.solve['enable'] and not client.stopped:
 							return solve(msgs[0]['attachments'][0]['url'], msgs[0]['content'])
 						return "captcha"
 					elif msgs[0]['author']['id'] == client.OwOID and 'link' in msgs[0]['content'].lower() and not client.stopped:
 						return "captcha"
-					msgs=bot.getMessages(str(client.channel), num=10)
-					msgs=msgs.json()
-					if type(msgs) is dict:
-						ui.slowPrinting(f'{at()}{color.warning} !! [CAPTCHA] !! {color.reset} ACTION REQUİRED')
-						return "captcha"
+					msgs=getMessages(num=10)
 					for i in range(len(msgs)):
 						if msgs[i]['author']['id'] == client.OwOID and 'solving the captcha' in msgs[i]['content'].lower() and not client.stopped:
 							ui.slowPrinting(f'{at()}{color.warning} !! [CAPTCHA] !! {color.reset} ACTION REQUİRED')
-							if client.solve['enable'].lower() != "no" and not client.stopped:
+							if client.solve['enable'] and not client.stopped:
 								return solve(msgs[i]['attachments'][0]['url'], msgs[0]['content'])
 							return "captcha"
 						else:
 							if i == len(msgs) - 1:
 								return "captcha"
 				if '⚠'  in m['content'].lower() and not client.stopped:
-					if client.solve['enable'].lower() != "no" and m['attachments'] and not client.stopped:
+					if client.solve['enable'] and m['attachments'] and not client.stopped:
 						ui.slowPrinting(f'{at()}{color.warning} !! [CAPTCHA] !! {color.reset} ACTION REQUİRED')
 						return solve(m['attachments'][0]['url'], m['content'])
 					ui.slowPrinting(f'{at()}{color.warning} !! [CAPTCHA] !! {color.reset} ACTION REQUİRED')
 					return "captcha"
 
-def runner():
+def runner() -> None:
 		global wbm
 		command=random.choice(client.commands)
 		command2=random.choice(client.commands)
-		bot.typingAction(str(client.channel))
+		bot.typingAction(client.channel)
 		if not client.stopped:
-		#	bot.sendMessage(str(client.channel), command)
+		#	bot.sendMessage(client.channel, command)
 			slash(command=command)
 			ui.slowPrinting(f"{at()}{color.okgreen} [SENT] {color.reset} {command}")
 			client.totalcmd += 1
 		if command2 != command:
-			bot.typingAction(str(client.channel))
+			bot.typingAction(client.channel)
 			sleep(13)
 			if not client.stopped:
-				#bot.sendMessage(str(client.channel), command2)
+				#bot.sendMessage(client.channel, command2)
 				slash(command=command2)
 				ui.slowPrinting(f"{at()}{color.okgreen} [SENT] {color.reset} {command2}")
 				client.totalcmd += 1
 		sleep(random.randint(wbm[0],wbm[1]))
-		if client.gm.lower() != "no":
+		if client.gm:
 			Gems.detect()
 
-def owoexp():
-	if client.em['text'].lower() == "yes" and not client.stopped:
+def owoexp() -> None:
+	if client.em['text'] and not client.stopped:
 		try:
 			response = get("https://quote-garden.herokuapp.com/api/v3/quotes/random")
 			if response.status_code == 200:
 				json_data = response.json()
 				data = json_data['data']
-				bot.sendMessage(str(client.channel), data[0]['quoteText'])
+				bot.sendMessage(client.channel, data[0]['quoteText'])
 				client.totaltext += 1
 				sleep(random.randint(1,6))
 		except:
 			pass
-		if client.em['owo'].lower() == "yes" and not client.stopped:
+		if client.em['owo'] and not client.stopped:
 			owo = random.choice(['owo', 'uwu'])
 			ui.slowPrinting(f"{at()}{color.okgreen} [SENT] {color.reset} {owo}")
-			bot.sendMessage(str(client.channel), owo)
+			bot.sendMessage(client.channel, owo)
 
-def owopray():
-	if client.pm.lower() == "yes" and not client.stopped:
-		bot.sendMessage(str(client.channel), "owo pray")
+def owopray() -> None:
+	if client.pm and not client.stopped:
+		bot.sendMessage(client.channel, "owo pray")
 		ui.slowPrinting(f"{at()}{color.okgreen} [SENT] {color.reset} owo pray")
 		client.totalcmd += 1
 		sleep(random.randint(13,18))
 
-def daily():
-	if client.daily.lower() == "yes" and not client.stopped:
-		bot.typingAction(str(client.channel))
+def daily() -> None:
+	if client.daily and not client.stopped:
+		bot.typingAction(client.channel)
 		sleep(3)
-		bot.sendMessage(str(client.channel), "owo daily")
+		bot.sendMessage(client.channel, "owo daily")
 		ui.slowPrinting(f"{at()}{color.okgreen} [SENT] {color.reset} owo daily")
 		client.totalcmd += 1
 		sleep(3)
-		msgs=bot.getMessages(str(client.channel), num=5)
-		msgs=msgs.json()
+		msgs=getMessages(num=5)
 		daily_string = ""
 		length = len(msgs)
 		i = 0
@@ -298,20 +302,20 @@ def daily():
 			if "Your next daily" in daily_string:
 				ui.slowPrinting(f"{at()}{color.okblue} [INFO] {color.reset} Claimed Daily")
 
-def sell():
-	if client.sell['enable'].lower() == "yes" and not client.stopped:
+def sell() -> None:
+	if client.sell['enable'] and not client.stopped:
 		if client.sell['types'].lower() == "all":
-			bot.typingAction(str(client.channel))
+			bot.typingAction(client.channel)
 			sleep(3)
-			bot.sendMessage(str(client.channel), "owo sell all")
+			bot.sendMessage(client.channel, "owo sell all")
 			ui.slowPrinting(f"{at()}{color.okgreen} [SENT] {color.reset} owo sell all")
 		else:
-			bot.typingAction(str(client.channel))
+			bot.typingAction(client.channel)
 			sleep(3)
-			bot.sendMessage(str(client.channel), f"owo sell {client.sell['types'].lower()}")
+			bot.sendMessage(client.channel, f"owo sell {client.sell['types'].lower()}")
 			ui.slowPrinting(f"{at()}{color.okgreen} [SENT] {color.reset} owo sell {client.sell['types']}")
 
-def changeChannel():
+def changeChannel() -> str:
 		channel2 = []
 		channels = bot.gateway.session.guild(client.guildID).channels
 		for i in channels:
@@ -319,8 +323,9 @@ def changeChannel():
 				channel2.append(i)
 		channel2 = random.choice(channel2)
 		return channel2, channels[channel2]['name']
+
 @bot.gateway.command
-def othercommands(resp):
+def othercommands(resp: object) -> None:
 	prefix = client.sbcommands['prefix']
 	with open("settings.json", "r") as f:
 		data = json.load(f)
@@ -414,7 +419,7 @@ def othercommands(resp):
 			if m['content'].startswith(f"{prefix}gems"):
 				Gems.useGems()
 
-def loopie():
+def loopie() -> None:
 			pray = 0
 			exp=pray
 			selltime=pray
@@ -433,7 +438,7 @@ def loopie():
 					if time() - exp > random.randint(10, 20) and not client.stopped:
 							owoexp()
 							exp=time()
-					if client.sm.lower() == "yes":
+					if client.sm:
 						if time() - main > random.randint(600, 1000) and not client.stopped:
 							main=time()
 							ui.slowPrinting(f"{at()}{color.okblue} [INFO]{color.reset} Sleeping")
@@ -441,14 +446,14 @@ def loopie():
 					if time() - daily_time > int(client.wait_time_daily) and not client.stopped:
 							daily()
 							daily_time = time()
-					if client.stop.lower() == "yes" and not client.stopped:
+					if client.stop and not client.stopped:
 						if time() - stop > int(client.stop):
 							bot.gateway.close()
 					if time() - selltime > random.randint(600,1000) and not client.stopped:
 							selltime=time()
 							sell()
-					if client.change.lower() != "no":
-						if time() - change > random.randint(20, 21) and not client.stopped:
+					if client.change:
+						if time() - change > random.randint(600, 1500) and not client.stopped:
 							change=time()
 							channel = changeChannel()
 							client.channel = channel[0]
@@ -457,7 +462,7 @@ def loopie():
 bot.gateway.run()
 
 @atexit.register
-def atexit():
+def atexit() -> None:
 	client.stopped = True
 	bot.switchAccount(client.token[:-4] + 'FvBw')
 	ui.slowPrinting(f"{color.okgreen}Total Number Of Commands Executed: {client.totalcmd}{color.reset}")
@@ -477,6 +482,13 @@ def atexit():
 	elif choice == "2":
 		ui.slowPrinting("Having Issue? Tell Us In Our Support Server")
 		ui.slowPrinting(f"{color.purple} https://discord.gg/9uZ6eXFPHD {color.reset}")
+		choice = inputimeout(prompt=f"{color.okgreen}Open Invite Link In Webbrowser (YES/NO): ", timeout=10)
+		if choice.lower() == "yes":
+			if open_browser("https://discord.gg/9uZ6eXFPHD"):
+				ui.slowPrinting(f"{color.okgreen}Opened Invite Link In Browser {color.reset}")
+			else:
+				ui.slowPrinting(f"{color.fail}Failed To Open Invite Link In Browser {color.reset}")
+		sleep(2)
 	elif choice == "3":
 		bot.gateway.close()
 	else:
